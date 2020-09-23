c4c6503a69bbe303933b3162d7e4405f40ed7a0f
js <joshtwoone@gmail.com>
2020-09-23 03:21:42 -0700

    adding brazil

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -15,7 +15,7 @@ class News_model extends CI_Model {
 
         private function get_url($url = FALSE) { // API KEY: 597116e2bbea465fa794a86c3d6708bc
             if ($_GET["country"]) {
-                $url = 'https://newsapi.org/v2/top-headlines?country=' + $_GET["country"] + '&apiKey=597116e2bbea465fa794a86c3d6708bc';
+                $url = 'https://newsapi.org/v2/top-headlines?country=' . $_GET["country"] . '&apiKey=597116e2bbea465fa794a86c3d6708bc';
                 $response = file_get_contents($url);
             }
             else $response = file_get_contents('https://newsapi.org/v2/top-headlines?country=co&apiKey=597116e2bbea465fa794a86c3d6708bc'); // country=br (Brazil), co (Colombia)
